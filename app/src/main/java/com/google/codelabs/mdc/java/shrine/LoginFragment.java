96d11000a9ff5838d9f4c91e43b6aa3ffa37413a
manofrancisco <francisco4martinsferreira@gmail.com>
2020-12-01 15:33:20 +0000

    commit

---
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/LoginFragment.java
@@ -0,0 +1,65 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+/**
+ * Fragment representing the login screen for the app.
+ */
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.login_fragment, container, false);
+        final TextInputLayout passwordTextInput = view.findViewById(R.id.password_text_input);
+        final TextInputEditText passwordEditText = view.findViewById(R.id.password_edit_text);
+        MaterialButton nextButton = view.findViewById(R.id.next_button);
+
+        // Set an error if the password is less than 8 characters.
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(getString(R.string.shr_error_password));
+                } else {
+                    passwordTextInput.setError(null); // Clear the error
+
+                    //The false parameter in navigateTo() tells the activity to not add the current fragment to the backstack,
+                    // so the user will not be able to return to the login screen using their back key.
+                    ((NavigationHost) getActivity()).navigateTo(new ProductGridFragment(), false);
+                }
+            }
+        });
+
+        // Clear the error once more than 8 characters are typed.
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (isPasswordValid(passwordEditText.getText())) {
+                    passwordTextInput.setError(null); //Clear the error
+                }
+                return false;
+            }
+        });
+        return view;
+    }
+
+
+    //DUMMY PASSWORD LOGIC
+    private boolean isPasswordValid(@Nullable Editable text) {
+        return text != null && text.length() >= 3;
+    }
+}
